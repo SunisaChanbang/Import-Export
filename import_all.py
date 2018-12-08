d1436240ffd11e1bb1940bfdf3855dda121bd0ef
Fai Fai <42977918+SunisaChanbang@users.noreply.github.com>
2018-12-08 23:38:04 +0700

    Update import_all.py

--- import_all.py
+++ import_all.py
@@ -1,10 +1,11 @@
 """ Import Calculator """
 from itertools import groupby
 def import_all():
-""" import_all is function sum product since 2015-2018
+""" import_all is function sum product since 2015-2018 
 	num23 = วัตถุดิบใช้ผลิตอาหารสัตว์
 	num27 = เชื้อเพลิงที่ได้จากแร่
 	num31 = ปุ๋ย
+	num38 = เคมีภัณฑ์เบ็ดเตล็ด
 	num39 = พลาสติกและของทำด้วยพลาสติก
 	num84 = เครื่องจักรและส่วนประกอบ
 	num85 = เครื่องอุปกรณ์ไฟฟ้าและส่วนประกอบ
@@ -12,5 +13,7 @@ def import_all():
 	num72 = เหล็กและเหล็กกล้า
 	num73 = ของทำด้วยเหล็กหรือเหล็กกล้า
 	num74 = ทองแดงละของทำด้วยทองแดง
+	num76 = อลูมิเนียมและของทำด้วยอลูมิเนียม
 	num90 = อุปกรณ์ที่ใช้ทางทัศนศาสตร์
-	num = อื่นๆ
+	num = อื่นๆ 
+"""
