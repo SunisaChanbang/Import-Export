ce571b5fdf0534b2af1f60e2c30969776985fa31
gusbellze <43022313+gusbellze@users.noreply.github.com>
2018-12-10 19:32:23 +0700

    Update and rename export_all.txt to export_all.py

--- export_all.py
+++ export_all.py
@@ -1,7 +1,8 @@
 """ export Calculator """
+import csv
 from itertools import groupby
 def export_all():
-""" export_all is function sum product since 2014-2018 
+    """ 'export_all is function sum product since 2014-2018' 
 	num10 = ข้าว
 	num16 = ของปรุงแต่งจากเนื้อสัตว์ ปลา
 	num20 = ของปรุงแต่งทำจากพืชผัก ผลไม้ ลูกนัต
@@ -13,6 +14,21 @@ def export_all():
 	num84 = เครื่องจักรและส่วนประกอบ
 	num85 = เครื่องอุปกรณ์ไฟฟ้าและส่วนประกอบ
 	num87 = ยานยนต์และส่วนประกอบ
-	
-	num = อื่นๆ 
-"""
+	num = อื่นๆ
+	thai_baht_export = มูลค่าเงินส่งออก
+    """
+    years = range(2557, 2561)
+    for year in years:
+        path = 'export%d.txt' % year
+        file = open(path)
+        data = csv.reader(file)
+        table = [row for row in data]
+        table[:4]
+
+    thai_baht_export = groupby(table, lambda x: x[1])
+    for key, group in thai_baht_export:
+        total_baht = 0
+        for item in group:
+            total_baht += int(item[2])
+            print(item[1], total_baht)
+export_all()
