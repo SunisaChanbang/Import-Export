285e3f02c68f65d1d785464099828f9fe0075011
Fai Fai <42977918+SunisaChanbang@users.noreply.github.com>
2018-12-10 19:38:42 +0700

    Update import_all.py

--- import_all.py
+++ import_all.py
@@ -1,19 +1,36 @@
 """ Import Calculator """
+import csv
 from itertools import groupby
 def import_all():
-""" import_all is function sum product since 2014-2018 
-	num23 = วัตถุดิบใช้ผลิตอาหารสัตว์
-	num27 = เชื้อเพลิงที่ได้จากแร่
-	num31 = ปุ๋ย
-	num38 = เคมีภัณฑ์เบ็ดเตล็ด
-	num39 = พลาสติกและของทำด้วยพลาสติก
-	num84 = เครื่องจักรและส่วนประกอบ
-	num85 = เครื่องอุปกรณ์ไฟฟ้าและส่วนประกอบ
-	num87 = ยานยนต์และส่วนประกอบ
-	num72 = เหล็กและเหล็กกล้า
-	num73 = ของทำด้วยเหล็กหรือเหล็กกล้า
-	num74 = ทองแดงละของทำด้วยทองแดง
-	num76 = อลูมิเนียมและของทำด้วยอลูมิเนียม
-	num90 = อุปกรณ์ที่ใช้ทางทัศนศาสตร์
-	num = อื่นๆ 
-"""
+    """ 'import_all is function sum product since 2014-2018'
+    num23 = วัตถุดิบใช้ผลิตอาหารสัตว์
+    num27 = เชื้อเพลิงที่ได้จากแร่
+    num31 = ปุ๋ย
+    num38 = เคมีภัณฑ์เบ็ดเตล็ด
+    num39 = พลาสติกและของทำด้วยพลาสติก
+    num84 = เครื่องจักรและส่วนประกอบ
+    num85 = เครื่องอุปกรณ์ไฟฟ้าและส่วนประกอบ
+    num87 = ยานยนต์และส่วนประกอบ
+    num72 = เหล็กและเหล็กกล้า
+    num73 = ของทำด้วยเหล็กหรือเหล็กกล้า
+    num74 = ทองแดงละของทำด้วยทองแดง
+    num76 = อลูมิเนียมและของทำด้วยอลูมิเนียม
+    num90 = อุปกรณ์ที่ใช้ทางทัศนศาสตร์
+    num = อื่นๆ
+    thai_baht_import = มูลค่านำเข้าเงินบาท
+    """
+    years = range(2557, 2561)
+    for year in years:
+        path = 'import%d.txt' % year
+        file = open(path)
+        data = csv.reader(file)
+        table = [row for row in data]
+        table[:4]
+
+    thai_baht_import = groupby(table, lambda x: x[1])
+    for key, group in thai_baht_import:
+        total_baht = 0
+        for item in group:
+            total_baht += int(item[2])
+            print(item[1], total_baht)
+import_all()
